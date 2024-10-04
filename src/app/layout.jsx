f5fb74c0b46d66a845e35edacca96a831b79ef9f
Shivam03-coder <shivam850anand@gmail.com>
2024-10-04 17:15:37 +0530

    done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import Header from "@/components/layouts/header/Header";
+import Header from "@/components/header";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
 
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav className="flex mx-auto bg-custom-blacktransparent-blue-gradient-t-b justify-between px-10 py-3">
+    <nav className="flex mx-auto bg-slate-900  justify-between px-10 py-3">
       <section>
         <div
-          className="text-2xl flex-center gap-4 font-semibold
-       text-greatBlue-400 border p-2 border-greatBlue-400"
+          className="text-2xl text-indigo-500 flex-center gap-4 font-semibold
+       text-greatBlue-400  p-2 "
         >
+          <Cuboid className="text-greatBlue-400" size={37} />
           CARDONA
-        <Cuboid className="text-greatBlue-400" size={27} />
         </div>
       </section>
       <div
